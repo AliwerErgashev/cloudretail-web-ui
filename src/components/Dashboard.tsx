73e1e76a059180bb6922d17a34a36de5fd1ca75a
Ravshan Samandarov <ravshansbox@gmail.com>
2021-10-29 16:31:40 +0500

    show current user name in dashboard page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,9 @@
 import { ComponentType } from 'react';
+import { useSelector } from 'react-redux';
 import { authorized } from '../common/authorized';
+import { State } from '../common/store';
 
 export const Dashboard: ComponentType = authorized(() => {
-  return <h1>Dashboard</h1>;
+  const auth = useSelector((state: State) => state.auth);
+  return <h1>You're {auth.user?.username}</h1>;
 });
