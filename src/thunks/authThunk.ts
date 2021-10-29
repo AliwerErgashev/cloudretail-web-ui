a340ad7070bb912f634b4dc6c749bd9b7bd48747
Ravshan Samandarov <ravshansbox@gmail.com>
2021-10-29 16:30:57 +0500

    open dashboard after successful sign in

--- src/thunks/authThunk.ts
+++ src/thunks/authThunk.ts
@@ -1,5 +1,6 @@
 import { Dispatch } from '@reduxjs/toolkit';
 import { tokenApi } from '../common/apis';
+import { history } from '../common/history';
 import { authActions, SignInRequestPayload } from '../reducers/authReducer';
 
 export const signIn = (payload: SignInRequestPayload) => async (dispatch: Dispatch) => {
@@ -7,6 +8,7 @@ export const signIn = (payload: SignInRequestPayload) => async (dispatch: Dispat
   try {
     const token = await tokenApi.create<string>(payload);
     dispatch(authActions.signInSuccess({ token, user: { username: payload.username } }));
+    history.push('/');
   } catch (error) {
     dispatch(authActions.signInFailure());
   }
