31560f6bd716d061324249e13ba0a9849fee5103
Ravshan Samandarov <ravshansbox@gmail.com>
2021-10-31 16:20:24 +0500

    update fetchJson: account http status code

--- src/common/fetchJson.ts
+++ src/common/fetchJson.ts
@@ -9,16 +9,26 @@ enum CONTENT_TYPES {
   JSON = 'application/json',
 }
 
+const isSuccess = (response: Response) => {
+  return response.status >= 200 && response.status < 300;
+};
+
 export const fetchJson = async <T = unknown>(method: Method, path: string, payload?: unknown) => {
   const headers = {
     [HEADERS.ACCEPTS]: CONTENT_TYPES.JSON,
     [HEADERS.CONTENT_TYPE]: CONTENT_TYPES.JSON,
   };
   const body = payload ? JSON.stringify(payload) : null;
-  const response = await fetch(path, {
-    method,
-    headers,
-    body,
-  });
-  return response.json() as Promise<T>;
+  const response = await fetch(path, { method, headers, body });
+  const text = await response.text();
+  try {
+    const json = JSON.parse(text);
+    if (isSuccess(response)) {
+      return json as T;
+    } else {
+      throw new Error(json.message);
+    }
+  } catch (error) {
+    throw new Error(text);
+  }
 };
