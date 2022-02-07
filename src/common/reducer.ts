29a8309c29bd0c899516b08aeb41967117a4d320
Ravshan Samandarov <ravshansbox@gmail.com>
2022-02-07 05:47:14 +0500

    bump deps

--- src/common/reducer.ts
+++ src/common/reducer.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 import { authReducer as auth } from '../reducers/authReducer';
 
 export const reducer = combineReducers({ auth });
